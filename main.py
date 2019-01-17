b710ae0686bb6cc33874409fbb3ebcda3ea561eb
Franco Colmenarez <francoacg1@gmail.com>
2019-01-17 08:39:42 -0400

    Using while loop for ask the client name

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 
 clients = 'pablo,ricardo,'
 
@@ -43,7 +44,20 @@ def search_client(client_name):
 
 
 def _get_client_name():
-	return input('What is the client name? ')
+	client_name = None
+
+	while not client_name:
+		client_name = input('What is the client name? ')
+
+		if client_name == 'exit':
+			client_name = None
+			break
+
+
+	if not client_name:
+		sys.exit()
+
+	return client_name
 
 
 def _print_welcome():
